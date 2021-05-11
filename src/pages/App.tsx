82317eeffaaca1b6ce2429181e6b4540ebe378d0
abdelaziz <abdelazizt320@gmail.com>
2021-05-12 01:17:50 +0200

    nft card ui added

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react'
 import { Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
 import GoogleAnalyticsReporter from '../components/analytics/GoogleAnalyticsReporter'
- import Header from '../components/Header'
+import Header from '../components/Header'
 import Polling from '../components/Header/Polling'
 import URLWarning from '../components/Header/URLWarning'
 import Popups from '../components/Popups'
@@ -10,7 +10,18 @@ import Web3ReactManager from '../components/Web3ReactManager'
 // import { ApplicationModal } from '../state/application/actions'
 // import { useModalOpen, useToggleModal } from '../state/application/hooks'
 import DarkModeQueryParamReader from '../theme/DarkModeQueryParamReader'
- 
+
+//  for  Card testing
+import NTFCard, { NftCardContent } from '../components/NFTcard/ntfcard'
+
+// DEMO NTF CARD CONTENT
+const nftCrd: NftCardContent = {
+  ntfImg: 'https://picsum.photos/200',
+  title: 'Apple Watch Series 4 GPS',
+  price: '16 ETH',
+  description: 'Redesigned from scratch and completely revised'
+}
+
 const AppWrapper = styled.div`
   display: flex;
   flex-flow: column;
@@ -47,9 +58,8 @@ const Marginer = styled.div`
   margin-top: 5rem;
 `
 
- 
-
 export default function App() {
+  const navigateToCard = () => {}
   return (
     <Suspense fallback={null}>
       <Route component={GoogleAnalyticsReporter} />
@@ -64,10 +74,14 @@ export default function App() {
           <Polling />
           <Web3ReactManager>
             <Switch>
-            
-              
-              
-             </Switch>
+
+              <NTFCard
+                cardContent={nftCrd}
+                navigateToCard={navigateToCard}
+                addToWhiteList={navigateToCard}
+                placeBide={navigateToCard}
+              ></NTFCard>
+            </Switch>
           </Web3ReactManager>
           <Marginer />
         </BodyWrapper>
