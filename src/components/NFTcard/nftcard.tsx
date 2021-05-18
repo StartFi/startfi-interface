34f4c74a6cb1b6bb9fc58ad952283fa5c7a39a3b
abdelaziz <abdelazizt320@gmail.com>
2021-05-18 22:06:05 +0200

    fix styling issue

--- src/components/NFTcard/nftcard.tsx
+++ src/components/NFTcard/nftcard.tsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-// import CardMedia from '@material-ui/core/CardMedia'
-import Icon from '@material-ui/core/Icon'
+import Path from '../../assets/svg/Path.svg'
 import { useStyles } from './nftcard.styles'
 import { NftButton } from '../Button/index'
-
 
 export interface NftCardContent {
   ntfImg: string
@@ -28,27 +23,26 @@ const NTFCard: React.FC<NftCardProps> = ({ cardContent, navigateToCard, addToWhi
   return (
     <div>
       <Card className={classes.card}>
-        <CardActionArea>
-          {/* <CardMedia className={classes.media}  component="img" image={cardContent.ntfImg} title="IMG" /> */}
-          <div className={classes.media}  >
-            <img src={cardContent.ntfImg}/>
+        <div className={classes.media}>
+          <img src={cardContent.ntfImg} />
+        </div>
+
+        <div onClick={() => navigateToCard(cardContent)}>
+          <div className={classes.price}>
+            <p>{cardContent.price}</p>
           </div>
+          <div>
+            <p className={classes.title}>{cardContent.title}</p>
+            <p className={classes.description}> {cardContent.description}</p>
+          </div>
+        </div>
 
-
-          <CardContent onClick={() => navigateToCard(cardContent)}>
-            <div className={classes.price}>
-              <p>{cardContent.price}</p>
-            </div>
-            <div>
-              <h2>{cardContent.title}</h2>
-              <p> {cardContent.description}</p>
-            </div>
-          </CardContent>
-        </CardActionArea>
-        <CardActions className={classes.action} disableSpacing={true}>
+        <div className={classes.action}>
           <div className={classes.whiteList}>
-            <Icon className={classes.icon}>favorite_border</Icon>
-            <NftButton onClick={() => addToWhiteList(cardContent)}>WhiteList</NftButton>
+            <img className={classes.icon} src={Path} />
+            <NftButton onClick={() => addToWhiteList(cardContent)} color='#000000'>
+              WHITELIST
+            </NftButton>
           </div>
           <div className={classes.bid}>
             <NftButton onClick={() => placeBid(cardContent)} color='#ffffff'>
@@ -56,7 +50,7 @@ const NTFCard: React.FC<NftCardProps> = ({ cardContent, navigateToCard, addToWhi
               place a bid
             </NftButton>
           </div>
-        </CardActions>
+        </div>
       </Card>
     </div>
   )
