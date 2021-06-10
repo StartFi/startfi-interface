3672ebaddfa40adac85de69ea6163ce053387a3a
abdelaziz <abdelazizt320@gmail.com>
2021-06-10 07:51:25 +0200

    modify styling

--- src/components/inventory/index.tsx
+++ src/components/inventory/index.tsx
@@ -1,11 +1,39 @@
 // import Column from 'components/Column'
-import Row from 'components/Row';
+import Row from 'components/Row'
 import React from 'react'
+import styled from 'styled-components'
 import CardHeader from './CardHeader'
 import Header from './Header'
-import {  Container } from './inventory.styles'
-import { InventoryCard } from './inventory.styles'
 import MiniCard from './MiniCard'
+
+const InventoryCard = styled.div`
+  height: 500px;
+  width: 92%;
+  margin: 0px auto 0px auto;
+  background-color: #ffffff;
+  border: 1px solid #e3e3e3;
+  box-shadow: 0px 0px 20px rgba(239, 239, 239, 0.25);
+
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 15px;
+    border: 1px solid #e3e3e3;
+    background-color: #efefef;
+    border-radius: 100px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border-radius: 100px;
+    background-color: #b5b5b5;
+  }
+`
+
+const Container = styled.div`
+  width: 100%;
+  background-color: #fafafa;
+  padding: 4vh 3.2vw;
+`
+
+// const CardContentContainer = styled.div``
 
 const Inventory = () => {
   return (
@@ -13,15 +41,13 @@ const Inventory = () => {
       <Header></Header>
       <CardHeader></CardHeader>
       <InventoryCard>
-        <Row padding="20px">
-          <MiniCard/>
-          <MiniCard/>
-          <MiniCard/>
-          <MiniCard/>
-          <MiniCard/>
+        <Row padding='20px'>
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+          <MiniCard />
+           {/* <MiniCard /> */}
         </Row>
-
-
       </InventoryCard>
     </Container>
   )
