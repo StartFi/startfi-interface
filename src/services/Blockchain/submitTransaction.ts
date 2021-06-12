fa8238b71468e72d2c71ae4c26298680525f8aac
yehia67 <yehia.tarek@indigen.com>
2021-06-12 20:10:18 +0200

    feat(transaction): create submit transaction servcie

---
+++ src/services/Blockchain/submitTransaction.ts
@@ -0,0 +1,28 @@
+import { useCallback } from 'react'
+import { Contract } from '@ethersproject/contracts'
+
+export const useSubmitTransaction = (): ((
+  methodsName: string,
+  args: Array<string> | undefined,
+  contract: Contract | null,
+  account: string | null,
+  library: any
+) => void) => {
+  return useCallback(
+    (
+      methodsName: string,
+      args: Array<string> | undefined,
+      contract: Contract | null,
+      account: string | null,
+      library: any
+    ) => {
+      const callData = contract?.interface.encodeFunctionData(methodsName, args)
+      return library?.getSigner().sendTransaction({
+        from: account ? account : undefined,
+        to: contract?.address,
+        data: callData
+      })
+    },
+    []
+  )
+}
