b176daad2c18fadc591de893dfcdf8e857f9c63e
Seif Yasser <seifyasser@icloud.com>
2021-06-13 21:58:00 +0200

    bug fix
    minting congrats and nft card material ui removal

--- src/components/MintingCongrats/mintingCongrats.tsx
+++ src/components/MintingCongrats/mintingCongrats.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { useStyles } from './mintingCongrats.styles'
+import { useTranslation } from 'react-i18next';
+import {  Container,Header,Text} from './mintingCongrats.styles'
 function MintingCongrats() {
-    const classes =useStyles()
-    return (
-        <div className={classes.container}>
-            <p className={classes.header}>Congratulations </p>
-            <p className={classes.text}>Your NFT Product will be reviewd by <span>Startfi</span> NFT Publishing team and will be Published in 48 hours</p>
 
-        </div>
+    const { t } = useTranslation()
+
+    return (
+        <Container>
+            <Header>   {t('congratulations')}</Header>
+           <Text> {t('nftReviewedBy')} <span>Startfi</span> {t('nftPublishing')}</Text>
+
+        </Container>
     );
 }
 
