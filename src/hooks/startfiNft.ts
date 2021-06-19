7fee6a6863811be6fc359b1d57cffa9bceb47158
yehia67 <yehia.tarek@indigen.com>
2021-06-19 14:10:49 +0200

    fix(transaction): toggle wallet modale if user isn't connected, block transaction submission if user isn't connected

--- src/hooks/startfiNft.ts
+++ src/hooks/startfiNft.ts
@@ -1,15 +1,39 @@
 import { useCallback } from 'react'
 import { useSubmitTransaction } from 'services/Blockchain/submitTransaction'
 import { Contract } from '@ethersproject/contracts'
+import { useWalletModalToggle } from 'state/application/hooks'
 
 export const useMint = (): ((
   address: string,
+  ipfsHash: string,
   contract: Contract | null,
   account: string | null,
-  library: any
+  library: any,
+  withRoyality: boolean,
+  share?: string,
+  base?: string
 ) => void) => {
   const mint = useSubmitTransaction()
-  return useCallback((address: string, contract: Contract | null, account: string | null, library: any) => {
-    mint('mint', [address], contract, account, library)
-  }, [])
+  const toggleWalletModal = useWalletModalToggle()
+  return useCallback(
+    (
+      address: string,
+      ipfsHash: string,
+      contract: Contract | null,
+      account: string | null,
+      library: any,
+      withRoyality: boolean,
+      share?: string,
+      base?: string
+    ) => {
+      if (!account) {
+        toggleWalletModal()
+        return
+      }
+      withRoyality
+        ? mint('mint', [address, ipfsHash], contract, account, library)
+        : mint('mint', [address, ipfsHash, share as string, base as string], contract, account, library)
+    },
+    [mint]
+  )
 }
