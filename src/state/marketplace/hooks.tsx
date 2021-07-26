0d5536770d9dd1c42ef480d8e784a053324ec7b6
Seif Yasser <seifyasser@icloud.com>
2021-07-26 20:02:27 +0200

    clear ipfs progress on minting bug

--- src/state/marketplace/hooks.tsx
+++ src/state/marketplace/hooks.tsx
@@ -23,6 +23,7 @@ import {
 } from './actions'
 import { usePopup } from 'state/application/hooks'
 import { Auction } from 'services/models/Auction'
+import { useClearIPFSProgress } from 'state/ipfs/hooks'
 
 export const useMarketplace = (): AuctionNFT[] => {
   return useSelector((state: AppState) => state.marketplace.marketplace)
@@ -107,10 +108,12 @@ export const useMintNFT = (): (() => void) => {
   const owner = useUserAddress()
   const popup = usePopup()
   const nft = useNFT()
+  const clearIPFSprogress = useClearIPFSProgress()
 
   return useCallback(() => {
     if (owner && nft) {
       dispatch(mintNFTAction({ ...nft, owner, issuer: owner, issueDate: new Date() }))
+      clearIPFSprogress()
     } else popup({ success: false, message: 'Connect wallet or no NFT data' })
   }, [nft, owner, popup, dispatch])
 }
