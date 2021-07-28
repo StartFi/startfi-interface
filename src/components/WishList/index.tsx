1f624df77af0194ae796b19946b13f911c44ee49
abdelaziz <abdelazizt320@gmail.com>
2021-07-28 21:21:09 +0200

    localization added to wishList

--- src/components/WishList/index.tsx
+++ src/components/WishList/index.tsx
@@ -1,6 +1,7 @@
 import { ButtonOutlined } from 'components/Button'
 import { ContainerCard } from 'components/Card'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { useHistory } from 'react-router-dom'
 import { AuctionNFT } from 'services/models/AuctionNFT'
 import { useUserWishList } from 'state/user/hooks'
@@ -10,13 +11,15 @@ import { NoListContainer } from './WishList.styles'
 
 
 const WishList = () => {
+  const { t } = useTranslation()
+
   const history = useHistory()
   const userWishList: AuctionNFT[] = useUserWishList()
 
   return (
     <ContainerCard height='703px' width='100%' borderRadius='8px 8px 0 0' id='ui'>
       <Text fontFamily='Roboto' fontSize='1rem' color='#000000' FontWeight='500' margin='1.875rem 0 0 3.5rem'>
-        WishList
+       {t('wishList')}
       </Text>
 
       {userWishList.length > 0 ? (
@@ -30,7 +33,7 @@ const WishList = () => {
       ) : (
         <NoListContainer>
           <Text fontFamily='Roboto' fontSize='1.125rem' color='#010101' FontWeight='500' margin='0px 0px 30px 0px'>
-            There Are No Assets Added to your wishlist
+           {t('noAsset')}
           </Text>
           <ButtonOutlined onClick={() => history.push('/marketplace/nfts')}>Explore</ButtonOutlined>
         </NoListContainer>
