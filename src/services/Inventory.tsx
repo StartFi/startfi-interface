21f6e7bd7cf38110b464259d35e127b232d5dc20
abdelaziz <abdelazizt320@gmail.com>
2021-09-02 13:27:07 +0200

    clean console.log

--- src/services/Inventory.tsx
+++ src/services/Inventory.tsx
@@ -4,9 +4,8 @@ import { Inventory } from "./models/Inventory";
 
 
 export const addInventoryItem= async (item:Inventory)=>{
-    console.log('save Inv service',item)
     const itemAdded  =await  addInventory(item)
-    console.log('added',itemAdded)
     const status = checkSuccess({ itemAdded })
     return { status, itemAdded,type:item.type  }
 }
+
