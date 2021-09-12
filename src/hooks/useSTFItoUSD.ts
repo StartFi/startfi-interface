3e517767feae5c6418c7d1c3731b55bcec373739
AbdelrhmanAmin <maxhd213@gmail.com>
2021-09-12 17:46:27 +0200

    fix (balance) move abbrev converter to utilis and use it in hook

--- src/hooks/useSTFItoUSD.ts
+++ src/hooks/useSTFItoUSD.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useStfiUsdPrice } from 'services/Blockchain/cryptoPrice'
-
+import abbrevToNumber from 'utils/abbrevToNumber'
 /**
  * @dev this function call the hook to get the token price in usd ( 1 SFTI =?? usd)
  * @notice we should only multiply the token price returned from the exchange by the amount the user entered but since the token is not listed yet, we are applying like a workaround though using ether simulation
@@ -10,19 +10,10 @@ export const useSTFItoUSD = (amount: number): number => {
   const [USD, setUSD] = useState<number>(amount)
   const stfiUsdPrice = useStfiUsdPrice()
   useEffect(() => {
+    amount = abbrevToNumber(amount)
     stfiUsdPrice().then(value => {
-      // Declared a stringified version of amount...Couldn't declare it inside of the condition block because of TypeScript error.
-      let amountStr = amount.toString()
-      // Check if amount is a string (e.g 100m) and convert the string 100(m) to a number 100(000000)
-      if(typeof amount === 'string'){
-        const abbrevConverter = {m: '000000', k: '000'}
-        const abbrev = amountStr.slice(-1)
-        amountStr = amountStr.replace(abbrev, abbrevConverter[abbrev])
-        amount = Number(amountStr)
-      }
       let total = value * amount
-      // Check if total is a decimal
-      if(total % 1 !== 0){
+      if (total % 1 !== 0) {
         total = Number(total.toFixed(4))
       }
       setUSD(total)
