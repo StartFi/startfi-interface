84c92a21772f198f4a6aa04a00659c342b0a55e2
abdelaziz <abdelazizt320@gmail.com>
2021-09-14 23:26:28 +0200

    fix(startifi stack hoox):now hook return reserve

--- src/hooks/blockchain-hooks/startfiStakes.ts
+++ src/hooks/blockchain-hooks/startfiStakes.ts
@@ -6,7 +6,6 @@ import { evaluateTransaction } from 'services/Blockchain/useEvaluateTransaction'
 import { useActiveWeb3React } from './useActiveWeb3React'
 import { updateStakeBalance, updateStackDepositState } from 'state/user/actions'
 import { useDispatch } from 'react-redux'
-import { utils } from 'ethers'
 
 export const useDeposit = (): ((user: string, amount: string | number) => any) => {
   const { account, library } = useActiveWeb3React()
@@ -44,7 +43,7 @@ export const useGetReserves = (): ((owner: string) => any) => {
     async (owner: string) => {
       try {
         const userReserved = await evaluateTransaction(contract, 'getReserves', [owner])
-        const reserved = utils.formatEther(userReserved)
+        const reserved = userReserved.toHexString()
 
         dispatch(updateStakeBalance({ stakeBalance: parseInt(reserved, 16) }))
         return reserved
