fb14c54142c65042866b3cd88aea7b1f99311e7e
abdelaziz <abdelazizt320@gmail.com>
2021-09-15 20:56:02 +0200

    fix(required stack):ui added

--- src/components/AddAuction/AuctionSale.tsx
+++ src/components/AddAuction/AuctionSale.tsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import InputSTFI from '../../UI/Inputs/InputSTFI'
 import { useAddAuction } from 'state/marketplace/hooks'
 
+import { Input } from 'UI/Inputs'
+
 const AuctionSale: React.FC = () => {
   const { auction, handleChange, missing } = useAddAuction()
+  // const
+  const [requiredStakes, setRequiredStakes] = useState(0)
+  const listQualifyPercentage = 1
+  const base = 100
+  console.log(auction)
+  useEffect(() => {
+    const listingPrice =auction?.listingPrice||0
+    if (auction) {
+      setRequiredStakes((listingPrice * listQualifyPercentage) / base)
+    }
+  }, [auction])
+
   return (
     <React.Fragment>
       <InputSTFI
-        name="listingPrice"
-        label="NFTprice"
+        name='listingPrice'
+        label='NFTprice'
         value={auction.listingPrice || 0}
         onChange={handleChange}
         error={missing.includes('listingPrice')}
+      />
+
+      <Input
+        question='requiredStakedDesc'
+        name='requiredStack'
+        label='Required Stack'
+        value={requiredStakes}
+        onChange={handleChange}
+        number
       />
     </React.Fragment>
   )
